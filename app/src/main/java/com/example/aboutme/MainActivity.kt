6624792568b4facbea9694f7ebc0b3d1fe5668f7
lorenzo <lrnzmd@gmail.com>
2019-10-11 11:32:51 +0200

    binding data

--- app/src/main/java/com/example/aboutme/MainActivity.kt
+++ app/src/main/java/com/example/aboutme/MainActivity.kt
@@ -5,9 +5,6 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import android.view.inputmethod.InputMethodManager
-import android.widget.Button
-import android.widget.EditText
-import android.widget.TextView
 import androidx.databinding.DataBindingUtil
 import com.example.aboutme.databinding.ActivityMainBinding
 
@@ -15,10 +12,14 @@ class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
 
+    private val myName: MyName = MyName("Lorenzo Amodeo")
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         // setContentView(R.layout.activity_main)
         binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+
+        binding.myName = myName
 
         //findViewById<Button>(R.id.done_button).setOnClickListener {
           binding.doneButton.setOnClickListener {
@@ -32,7 +33,8 @@ class MainActivity : AppCompatActivity() {
     private fun addNickname(view: View) {
 
     binding.apply {
-        binding.nicknameText.text = binding.nicknameEdit.text
+       // binding.nicknameText.text = binding.nicknameEdit.text
+        myName?.nickname= nicknameEdit.text.toString()
         invalidateAll()
         binding.nicknameEdit.visibility = View.GONE
         binding.doneButton.visibility = View.GONE
